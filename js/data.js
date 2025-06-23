8de7961bef892b721cb929de03a2c1686f02eb7d
ruby <rubyf2e@gmail.com>
2025-06-23 11:59:07 +0800

    更新設定檔以反映專案名稱，調整樣式以改善頁面外觀，並新增電影詳情按鈕及即將上映標籤

--- js/data.js
+++ js/data.js
@@ -1,26 +1,30 @@
 fetch('data/content.json')
-    .then(res => res.json())
-    .then(data => {
-        const popularEl = document.getElementById('popular-movies');
-        const comingEl = document.getElementById('coming-soon-movies');
+  .then(res => res.json())
+  .then(data => {
+    const popularEl = document.getElementById('popular-movies');
+    const comingEl = document.getElementById('coming-soon-movies');
 
-        data.popular.forEach(movie => {
-            popularEl.innerHTML += `
+    data.popular.forEach(movie => {
+      popularEl.innerHTML += `
         <div class="movie">
           <h3>${movie.title} <span>${movie.rating}</span></h3>
           <p>${movie.year}｜${movie.genre}</p>
           <p>${movie.description}</p>
+          <button>查看詳情</button>
         </div>
       `;
-        });
+    });
 
-        data.comingSoon.forEach(movie => {
-            comingEl.innerHTML += `
-        <div class="movie">
+    data.comingSoon.forEach(movie => {
+      comingEl.innerHTML += `
+        <div class="movie upcoming">
+          <span class="badge">即將上映</span>
           <h3>${movie.title}</h3>
           <p>上映日期: ${movie.releaseDate}｜${movie.genre}</p>
           <p>${movie.description}</p>
+          <div class="stars">★★★★★</div>
+          <button>設定提醒</button>
         </div>
       `;
-        });
     });
+  });
