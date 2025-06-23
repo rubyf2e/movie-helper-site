610d3bfa6fe6945cf19da5994348a7100fa47f8d
ruby <rubyf2e@gmail.com>
2025-06-23 10:51:34 +0800

    新增樣式、內容和數據處理，包含電影資訊和即將上映的電影

--- js/data.js
+++ js/data.js
@@ -1,1 +1,26 @@
-...
+fetch('data/content.json')
+    .then(res => res.json())
+    .then(data => {
+        const popularEl = document.getElementById('popular-movies');
+        const comingEl = document.getElementById('coming-soon-movies');
+
+        data.popular.forEach(movie => {
+            popularEl.innerHTML += `
+        <div class="movie">
+          <h3>${movie.title} <span>${movie.rating}</span></h3>
+          <p>${movie.year}｜${movie.genre}</p>
+          <p>${movie.description}</p>
+        </div>
+      `;
+        });
+
+        data.comingSoon.forEach(movie => {
+            comingEl.innerHTML += `
+        <div class="movie">
+          <h3>${movie.title}</h3>
+          <p>上映日期: ${movie.releaseDate}｜${movie.genre}</p>
+          <p>${movie.description}</p>
+        </div>
+      `;
+        });
+    });
